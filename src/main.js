e54ebb5877ff8db814baab38c9fadc60e4ed19ce
Christian Isaman <christianisaman@outlook.com>
2025-07-12 07:58:42 +0800

    refactor main function to destructure liquidity data for clarity

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@ import { getLiq } from "./getLiq.js";
 import { formatToDollars } from "./util/formatToDollars.js";
 
 async function main() {
-  const liq = await getLiq();
+  const { LpPositions, unclaimedFees, netLpValue, netIdleValue } = await getLiq();
 
-  liq.LpPositions.forEach(({ mintA, mintB, unclaimedFee }) => {
+  LpPositions.forEach(({ mintA, mintB, unclaimedFee }) => {
     console.log(`${mintA.symbol}: ${mintA.percent}%`);
     console.log(`${mintB.symbol}: ${mintB.percent}%`);
     console.log(`Fee: ${formatToDollars(unclaimedFee)}\n`);
   });
-  console.log(`Unclaimed Fees: ${formatToDollars(liq.unclaimedFees)}`);
-  console.log(`Value: ${formatToDollars(liq.netLpValue + liq.netIdleValue + liq.unclaimedFees)}`);
+  console.log(`Unclaimed Fees: ${formatToDollars(unclaimedFees)}`);
+  console.log(`Value: ${formatToDollars(netLpValue + netIdleValue + unclaimedFees)}`);
 }
 
 main();
